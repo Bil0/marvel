9a71ceb3ebd4c31e57d2ca0f34ab1477440fe631
Redouane Lakrache <red-0ne@smarty-concept.com>
2018-02-21 01:15:04 +0100

    chore(config): copy environment to environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'f6ef908792f697973acc37c5f0f89c4d',
+  apiHost: {
+    protocol: 'https',
+    name: 'gateway.marvel.com',
+    port: 433
+  }
 };
