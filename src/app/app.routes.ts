b30a5c52da0cbbae838eff080e7db7739b5c0030
Redouane Lakrache <red-0ne@smarty-concept.com>
2018-02-22 15:46:24 +0100

    feat: implement redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { HeroResolver } from './services/hero.resolver/hero.resolver';
 import { FavoritesResolver } from './services/favorites.resolver/favorites.resolver';
 
 export const AppRoutes: Routes = [
+  { path: '', redirectTo: 'heroes' },
   { path: 'heroes', component: HeroesListComponent, resolve: { heroes: HeroesListResolver } },
   { path: 'heroes/:page', component: HeroesListComponent, resolve: { heroes: HeroesListResolver } },
   { path: 'hero/:id', component: HeroDetailsComponent, resolve: { hero: HeroResolver }, outlet: 'dialog' },
